4aaccf2005c24ca98c8bbf9a52b9fa98d28a02f5
Uliana Goronkova <ulianahal@gmail.com>
2025-09-06 19:22:42 +0300

    Update 5_hw.py

--- 5_hw.py
+++ 5_hw.py
@@ -1,12 +1,23 @@
 ДЗ 5/1
 
+if__name__=="__main__""
+button = Button("КнопкиЭ")
+input_field = Input("Поля")
+title = Title("Заголовок")
+link = Link("Ссылки")
+
+    print(button.check_text())
+    print(input_field.check_text())
+    print(title.check_text())
+    print(link.check_text())
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 def check_elements():
 
 driver = webdriver.Chrome()
-driver.get("https://saucedemo.com/")
+driver.get("https://www.saucedemo.com/")
 
 username_field = driver.find_element(By.ID, "user.name")
 password_field = driver.find_element(By.ID, "user.password")
